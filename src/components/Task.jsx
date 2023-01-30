0c42a7848c7d2df020d8dca46cfc4518cc035f13
nnesteruk <119810364+nnesteruk@users.noreply.github.com>
2023-01-30 22:58:17 +0300

    Add files via upload

---
+++ src/components/Task.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { TaskInput } from './TaskInput';
+
+export const Task = ({ item, doneTask, removeTask, editTask }) => {
+  const [edit, setEdit] = useState(null);
+
+  const submitUpdate = (id, newTitle) => {
+    editTask(id, newTitle);
+    setEdit(null);
+  };
+
+  if (edit) {
+    return <TaskInput submitUpdate={submitUpdate} edit={edit} setEdit={setEdit} />;
+  }
+
+  const className = 'todo-list__task ' + (item.isCompleted ? ' task-done' : '');
+
+  return (
+    <div className={className}>
+      <li onClick={() => doneTask(item.id)} key={item.id}>
+        {item.title}
+      </li>
+      <div className="task__buttons">
+        <p
+          className="task__buttons-action"
+          onClick={() =>
+            setEdit({ id: item.id, title: item.title, isCompleted: item.isCompleted })
+          }>
+          🖉
+        </p>
+        <p className="task__buttons-action" onClick={() => removeTask(item.id)}>
+          ❌
+        </p>
+      </div>
+    </div>
+  );
+};
